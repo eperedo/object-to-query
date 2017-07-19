6a0f3e712e34128017109db827069d86ceedb5e7
Eduardo Peredo <jrperedo@gmail.com>
2017-07-18 21:31:38 -0500

    Added basic transformation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 function transform(obj) {
-	return 'query string';
+	if (obj && typeof obj === 'object') {
+		const keys = Object.keys(obj);
+		// const keysToConvert = keys.filter((kf) => {
+		//   return notInclude.indexOf(kf) === -1;
+		// })
+		const qs = keys.map((key) => {
+			const currentKey = obj[key];
+			return `&${key}=${currentKey}`;
+		});
+		return qs.join('');
+	} else {
+		return '';
+	}
 }
 
 module.exports = transform;
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,20 @@
 const test = require('tape');
 const transform = require('./index');
 
+test('should return an empty string if parameter is not an object', (assert) => {
+
+	const resultWithNull = transform(null);
+	const resultWithEmpty = transform('');
+	const resultWithUndefined = transform('');
+
+	assert.equal(resultWithNull, '', 'must return an empty string');
+	assert.equal(resultWithEmpty, '', 'must return an empty string');
+	assert.equal(resultWithUndefined, '', 'must return an empty string');
+
+	assert.end();
+
+});
+
 test('should pass', (assert) => {
 
 	const objectTest = {
@@ -10,7 +24,7 @@ test('should pass', (assert) => {
 
 	const result = transform(objectTest);
 
-	assert.ok(result, 'must return an string');
+	assert.equal(result, '&id=1&name=Eduardo', 'should generate a query string with 2 keys');
 
 	assert.end();
 
